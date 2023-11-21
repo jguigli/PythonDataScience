5711215c3250f0a31cd2952883e0851829898cf3
GUIGLI <joel.guigli@gmail.com>
2023-11-21 18:12:19 +0100

    debut ex04p00

---
+++ Python0/ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+import math
+from types import NoneType
+
+def NULL_not_found(object: any) -> int:
+	if isinstance(object, NoneType):
+		print("Nothing:", object, type(object))
+	elif isinstance(object, float) and object != object:
+		print("Cheese:", object, type(object))
+	elif isinstance(object, int) and object == 0:
+		print("Zero:", object, type(object))
+	elif isinstance(object, str) and object == "":
+		print("Empty:", object, type(object))
+	elif isinstance(object, bool):
+		print("Fake:", object, type(object))
+	else:
+		print("Type not Found")
+	return 1
