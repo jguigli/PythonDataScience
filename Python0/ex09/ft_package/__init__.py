7f52bf61e28f8d6cb612da852353dfc62c25c31f
Joel GUIGLI <jguigli@e2r13p4.clusters.42paris.fr>
2023-11-30 13:11:36 +0100

    modif p0 ex09 word->value

--- Python0/ex09/ft_package/__init__.py
+++ Python0/ex09/ft_package/__init__.py
@@ -2,7 +2,7 @@ def count_in_list(lst: list, value) -> int:
     """Retourne le nombre de value contenu dans lst"""
     result = 0
     try:
-        result = lst.count(word)
+        result = lst.count(value)
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
     return result
