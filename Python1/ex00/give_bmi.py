5b661203d00f57f78a4fbe5b46562f9a8db1d1ee
Joel GUIGLI <jguigli@e2r13p4.clusters.42paris.fr>
2023-11-30 17:39:33 +0100

    p1 ex00 presque bon

--- Python1/ex00/give_bmi.py
+++ Python1/ex00/give_bmi.py
@@ -3,11 +3,28 @@ import numpy as np
 
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
     """DOC HERE"""
-    # manage error
-    bmi = [w / (h ** 2) for h, w in zip(height, weight)]
-    np.array(heigh)
-    return bmi
+    if not all(isinstance(h, (int, float)) for h in height):
+        raise TypeError("Elements list must be int or float")
+    
+    if not all(isinstance(w, (int, float)) for w in weight):
+        raise TypeError("Elements list must be int or float")
+    
+    if len(height) != len(weight):
+        raise ValueError("Lists must have the same length")
+    
+    height_bmi = np.array(height)
+    weight_bmi = np.array(weight)
+
+    bmi = weight_bmi / height_bmi ** 2
+    return bmi.tolist()
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
     """DOC HERE"""
-    limit = 
+    if not all(isinstance(v, (int, float)) for v in bmi):
+        raise TypeError("Elements list must be int or float")
+    if not isinstance(limit, int):
+        raise TypeError("Limit must be int")
+        
+    bmi_np = np.array(bmi)
+    bmi_bool = bmi_np > limit
+    return bmi_bool.tolist()
