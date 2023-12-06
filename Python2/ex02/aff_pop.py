812d828f2e6f2065e9bf1d781a1313558d1f6c89
Joel GUIGLI <jguigli@e2r13p4.clusters.42paris.fr>
2023-12-06 17:06:53 +0100

    p1 ex01 ok, ex02 encours

--- Python2/ex02/aff_pop.py
+++ Python2/ex02/aff_pop.py
@@ -4,19 +4,20 @@ import pandas as pd
 
 
 def main():
-    """Get data csv and display a graph about life expectancy in France"""
+    """Get data csv and display a graph about life population in France and Belgium"""
     try:
         data = load("life_expectancy_years.csv")
         data_france = data.loc[data['country'] == 'France']
+        print()
 
         years = data_france.columns[1:].values.astype('int')
         life_expectancy = data_france.iloc[0, 1:].values
 
-        plt.plot(years, life_expectancy, linestyle='-')
-        plt.title('France Life expectancy Projections')
-        plt.xlabel('Years')
-        plt.ylabel('Life excpectancy')
-        plt.show()
+        # plt.plot(years, life_expectancy, linestyle='-')
+        # plt.title('France Life expectancy Projections')
+        # plt.xlabel('Years')
+        # plt.ylabel('Life excpectancy')
+        # plt.show()
     except Exception as e:
         print(f"Error handling: {str(e)}")
         return
