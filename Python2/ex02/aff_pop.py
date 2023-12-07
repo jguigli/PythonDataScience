0e55923c36bcf4d683233f7288ab4719266b7567
Joel GUIGLI <jguigli@e2r13p4.clusters.42paris.fr>
2023-12-07 16:29:13 +0100

    tjrs pareil

--- Python2/ex02/aff_pop.py
+++ Python2/ex02/aff_pop.py
@@ -3,21 +3,32 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+def remove_million(value):
+    return value.rstrip('M')
+
 def main():
     """Get data csv and display a graph about population in France and China betzeen 1800 and 2050"""
     try:
         data = load("population_total.csv")
-        # data_france_china = data.loc[data['country'].isin(['France', 'China'])]
-        # print(data_france_china)
+        data_france = data.loc[data['country'] == 'France']
+        data_spain = data.loc[data['country'] == 'Spain']
+        # print(data_france)
+        # print(data_spain)
 
-        # years = data_france_china[(data_france_china.columns.astype(int) <= 2050)]
-        # population = data_france_china.iloc[0:1, 1:].values
+        years = data.loc[:, '1800':'2050'].columns.tolist()
 
-        # plt.plot(years, population, linestyle='-')
-        # plt.title('Population Projections')
-        # plt.xlabel('Year')
-        # plt.ylabel('Population')
-        # plt.show()
+        france_population = data_france.loc[:, '1800':'2050'].map(remove_million).values.tolist()
+        spain_population = data_spain.loc[:, '1800':'2050'].map(remove_million).values.tolist()
+
+        # print(france_population)
+        # print(spain_population)
+
+        plt.plot(years, france_population, label='France', linestyle='-')
+        plt.plot(years, spain_population, label='Spain', linestyle='-')
+        plt.title('Population Projections')
+        plt.xlabel('Year')
+        plt.ylabel('Population')
+        plt.show()
     except Exception as e:
         print(f"Error handling: {str(e)}")
         return
